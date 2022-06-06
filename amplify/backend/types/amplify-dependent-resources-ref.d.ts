d781d339d111c9cc801166b91af9813133e57ed9
fhm013 <fahmi.hakim@devstack.com.au>
2022-06-06 10:05:38 +0700

    fix aws

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -19,41 +19,6 @@ export type AmplifyDependentResourcesAttributes = {
             "Arn": "string",
             "LambdaExecutionRole": "string",
             "Region": "string"
-        },
-        "employeeTableTriggere7f63298": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
-        "employeeApiLambda": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
-        "usersHandler": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        }
-    },
-    "storage": {
-        "employeeTable": {
-            "Name": "string",
-            "Arn": "string",
-            "StreamArn": "string",
-            "PartitionKeyName": "string",
-            "PartitionKeyType": "string",
-            "Region": "string"
-        }
-    },
-    "api": {
-        "userApi": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
         }
     }
 }
--- src/App.js
+++ src/App.js
@@ -3,18 +3,59 @@ import "./App.css";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import { API } from "aws-amplify";
+import axios from "axios";
 
 function App({ signOut, user }) {
-  const myAPI = "userApi";
+  const myAPI =
+    "https://05dvurz1mh.execute-api.ap-southeast-1.amazonaws.com/v1";
   const path = "/users";
 
   const [input, setInput] = useState("");
   const [customers, setCustomers] = useState([]);
+  const [name, setName] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileInput = (e) => {
+    // handle validations
+    const file = e.target.files[0];
+    if (file.size < 1024) setSelectedFile(e.target.files[0]);
+  };
+
+  const submitForm = async () => {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("file", selectedFile);
+
+    const payload = await API.post("employee", "add", formData);
+    // axios
+    //   .post(UPLOAD_URL, formData)
+    //   .then((res) => {
+    //     alert("File Upload success");
+    //   })
+    //   .catch((err) => alert("File Upload Error"));
+  };
+
+  React.useEffect(() => {
+    getUsers();
+  }, [])
+  
 
   //Function to fetch from our backend and update customers array
-  function getCustomer(e) {
-    let customerId = e.input;
-    API.get(myAPI, path + "/" + customerId)
+  function getUsers(e) {
+    // let customerId = e.input;
+    axios.defaults.baseURL = myAPI;
+    // axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+    // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+    axios
+      .get(path, {
+        headers: {
+          "Content-Type": "application/json;",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+          "Access-Control-Allow-Headers":
+            "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        },
+      })
       .then((response) => {
         console.log(response);
         let newCustomers = [...customers];
@@ -28,97 +69,52 @@ function App({ signOut, user }) {
 
   return (
     <div className="App">
-      <h1>Super Simple React App</h1>
-      <div>
-        <input
-          placeholder="customer id"
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-      </div>
-      <br />
-      <button onClick={() => getCustomer({ input })}>
-        Get Customer From Backend
-      </button>
-
-      <h2 style={{ visibility: customers.length > 0 ? "visible" : "hidden" }}>
-        Response
-      </h2>
-      {customers.map((thisCustomer, index) => {
-        return (
-          <div key={thisCustomer.userId}>
-            <span>
-              <b>CustomerId:</b> {thisCustomer.userId} - <b>CustomerName</b>
-              : {thisCustomer.userName}
-            </span>
-          </div>
-        );
-      })}
-      <div>
-      <div>
-        <button onClick={signOut}>Signout</button>
-      </div>
-      </div>
+      <header className="App-header">
+        <h1>Serverless App</h1>
+      </header>
+      <article>
+        <section>
+          <h3>Upload profile picture</h3>
+          <form>
+            <div>
+              <input
+                type="text"
+                placeholder="File title"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type="file"
+                value={selectedFile}
+                onChange={handleFileInput}
+              />
+            </div>
+            <div>
+              <button onClick={submitForm}>Upload</button>
+            </div>
+          </form>
+        </section>
+        <section>
+          <h3>Data</h3>
+          {customers.map((thisCustomer, index) => {
+            return (
+              <div key={thisCustomer.userId}>
+                <span>
+                  <b>CustomerId:</b> {thisCustomer.userId} - <b>CustomerName</b>
+                  : {thisCustomer.userName}
+                </span>
+              </div>
+            );
+          })}
+        </section>
+      </article>
+      <footer>
+        <div>
+          <button onClick={signOut}>Signout</button>
+        </div>
+      </footer>
     </div>
   );
-
-  // const [name, setName] = useState("");
-  // const [selectedFile, setSelectedFile] = useState(null);
-
-  // const handleFileInput = (e) => {
-  //   // handle validations
-  //   const file = e.target.files[0];
-  //   if (file.size < 1024) setSelectedFile(e.target.files[0]);
-  // };
-
-  // const submitForm = async () => {
-  //   const formData = new FormData();
-  //   formData.append("name", name);
-  //   formData.append("file", selectedFile);
-
-  //   const payload = await API.post("employee", "add", formData);
-  //   // axios
-  //   //   .post(UPLOAD_URL, formData)
-  //   //   .then((res) => {
-  //   //     alert("File Upload success");
-  //   //   })
-  //   //   .catch((err) => alert("File Upload Error"));
-  // };
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <h1>Serverless App</h1>
-  //     </header>
-  //     <article>
-  //       <section>
-  //         <form>
-  //           <div>
-  //             <input
-  //               type="text"
-  //               placeholder="File title"
-  //               value={name}
-  //               onChange={(e) => setName(e.target.value)}
-  //             />
-  //             <input
-  //               type="file"
-  //               value={selectedFile}
-  //               onChange={handleFileInput}
-  //             />
-  //           </div>
-  //           <div>
-  //             <button onClick={submitForm}>Upload</button>
-  //           </div>
-  //         </form>
-  //       </section>
-  //     </article>
-  //     <footer>
-  //       <div>
-  //         <button onClick={signOut}>Signout</button>
-  //       </div>
-  //     </footer>
-  //   </div>
-  // );
 }
 
 export default withAuthenticator(App);
